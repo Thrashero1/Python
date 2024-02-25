6e3450625f45cd571905e0e0183250df79293b8e
David Borg <maniadave@gmail.com>
2024-02-25 12:05:36 +0100

    day19

--- day19/turtle_race.py
+++ day19/turtle_race.py
@@ -4,34 +4,47 @@ screen = Screen()
 colours = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
 turtles = ['timred', 'timblue', 'timgreen', 'timyellow', 'timpurple', 'timorange']
 turtle_objects = []
-screen.setup(500, 500)
+x_size = 700
+y_size = 300
+screen.setup(x_size, y_size)
+
+race_going = False
 
 winner = screen.textinput('Turtle Racing!', 'Which colour do you think will win?')
 
+if winner:
+    race_going = True
+
 for x, y in enumerate(turtles):
-    y = Turtle()
-    y.shape('turtle')
+    y = Turtle(shape = 'turtle')
     y.color(colours[x])
-    y.speed(6)
+    y.speed(0)
     y.pu()
     turtle_objects.append(y)
 
 def start_positions(turtle_list):
-    space = 500 / 7
-    start_y = 250 - space
+    space = y_size / 7
+    start_y = (y_size/2) - space
     for turtle in turtle_list:
-        turtle.teleport(-240, start_y)
+        turtle.teleport(-x_size/2 - 10, start_y)
         start_y -= space
         
-
+start_positions(turtle_objects)
 
 def moveturtles(turtle_list):
-    for turtles in enumerate(turtle_list):
-        move = rand.randint(1, 300)
+    for turtles in turtle_list:
+        move = rand.randint(1, 10)
         turtles.fd(move)
 
-for x in range(1,10):
+while race_going:
     moveturtles(turtle_objects)
+    for turtle in turtle_objects:
+        if turtle.xcor() > x_size / 2 - 20:
+            if winner == turtle.pencolor():
+                print(f'You have won! {winner} is the race winner!')
+            else:
+                print(f'Oh no {turtle.pencolor()} has won the race, you did not win!')
+            race_going = False
 
-start_positions(turtle_objects)
+
 screen.exitonclick()
